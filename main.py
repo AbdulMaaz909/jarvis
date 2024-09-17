0f07ed21b361002e5165ee7a3a916ba25ae116a0
AbdulMaaz909 <155896522+AbdulMaaz909@users.noreply.github.com>
2024-09-17 23:21:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+
+#pip install pocketspinx
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi = "99dfcc595cb0635be520a70730e65802"
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processcommand(c):
+    if "open google" in c.lower ():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower ():
+        webbrowser.open("https://youtube.com")
+    elif "open facebook" in c.lower ():
+        webbrowser.open("https://facebook.com")
+    elif "open github" in c.lower ():
+        webbrowser.open("https://github.com")
+    elif "open playstore" in c.lower ():
+        webbrowser.open("https://playstore")
+    elif c.lower().startswith("play"):
+        song= c.lower().split(" ")[1]  #play skyfall ['play','pathan'] indexing 1 (split means space)
+        link= musicLibrary.music[song]
+        webbrowser.open(link)
+    
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsdata.io/api/1/latest?apikey={newsapi}")
+        if r.status_code ==200:
+            #parse the JSON response
+            data= r.json()
+
+            #extrat the articles
+            articles = data.get('articles',[])
+
+            for article in articles:
+                speak(article['title']) 
+
+    
+    
+
+if __name__=="__main__":
+    speak("Initializing Jarvis...")
+    while True:
+        r =sr.Recognizer()
+
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("listening...")
+                audio = r.listen(source,timeout=3,phrase_time_limit=2)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yes Abdul Maaz")
+                #listen for command 
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processcommand(command)
+
+        except Exception as e:
+            print("Error; {0}".format(e))
+
+
+
+'''
+ERROR 
+1.not found pyaudio : install pyaudio
+2. no named module distutils : install setuptools
+
+'''
+
+# 99dfcc595cb0635be520a70730e65802
---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "mashooq": "https://www.youtube.com/watch?v=BuAno6LzoJY&list=RDBuAno6LzoJY&start_radio=1 ",
+    "pathan" : "https://www.youtube.com/watch?v=YxWlaYCA8MU" ,  
+    "fitoor" : "https://www.youtube.com/watch?v=gDybddrGpeA ",
+    "mashallah":"https://www.youtube.com/watch?v=9_BUZaTcozs"
+    
+}
+
+
